629aa45f37bb539e77acaa739055e629bcaff15d
Christian Alexander Yábar Mejer <chm003@edu.zealand.dk>
2024-10-08 14:41:50 +0200

    vuejsDemo

---
+++ start/indexScript.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            productName: "book of spaceships flying with mouse",
+            productDescription: "Spaceships flying to space with mouse inside",
+            imageSouce: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBmwOlzFMQZK-5pEB76Fe3T8fzpe1TrJEFPA&s",
+            productImageDescription: 'An astronaut floats outside the window while you sit in comfort',
+            productImageStyle: {
+                'border-radius': '15px'
+            }
+        }
+    }
+})
